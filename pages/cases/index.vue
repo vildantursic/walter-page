f60fa5f2e0ea2592ffabd4156160e54f52a3a340
Irma Rustemovic <irma.rustemovic@walter.ba>
2018-04-17 09:26:04 +0200

    first commit

---
+++ pages/cases/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container">
+    <header></header>
+    <div>
+
+    </div>
+  </section>
+</template>
+
+<script>
+  import header from '~/components/partials/header.vue'
+
+  export default {
+    components: {
+      header
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="container">
+    <div>
+
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style>
+</style>
