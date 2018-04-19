015d85ecd4a58d2ffe80285b241041c7d0f3f053
Irma Rustemovic <irma.rustemovic@walter.ba>
2018-04-19 14:37:58 +0200

    slider structure component

--- components/AppSlider.vue
+++ components/AppSlider.vue
@@ -1,16 +1,94 @@
 <template>
-  <div>
-
-  </div>
+  <md-card>
+    <md-card-actions>
+      <md-button class="md-icon-button"
+                 target="_blank"
+                 href="https://github.com/surmon-china/vue-awesome-swiper/blob/master/examples/30-thumbs-gallery.vue">
+        <md-icon>code</md-icon>
+      </md-button>
+    </md-card-actions>
+    <md-card-media  style="height: 500px">
+      <!-- swiper1 -->
+      <swiper :options="swiperOptionTop" class="gallery-top" ref="swiperTop">
+        <swiper-slide class="slide-1"></swiper-slide>
+        <swiper-slide class="slide-2"></swiper-slide>
+        <swiper-slide class="slide-3"></swiper-slide>
+        <div class="swiper-button-next swiper-button-white" slot="button-next"></div>
+        <div class="swiper-button-prev swiper-button-white" slot="button-prev"></div>
+      </swiper>
+      <!-- swiper2 Thumbs -->
+      <swiper :options="swiperOptionThumbs" class="gallery-thumbs" ref="swiperThumbs">
+        <swiper-slide class="slide-1"></swiper-slide>
+        <swiper-slide class="slide-2"></swiper-slide>
+        <swiper-slide class="slide-3"></swiper-slide>
+      </swiper>
+    </md-card-media>
+  </md-card>
 </template>
 
 <script>
   export default {
-    components: {
+    data() {
+      return {
+        swiperOptionTop: {
+          spaceBetween: 10,
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+          }
+        },
+        swiperOptionThumbs: {
+          spaceBetween: 10,
+          centeredSlides: true,
+          slidesPerView: 'auto',
+          touchRatio: 0.2,
+          slideToClickedSlide: true
+        }
+      }
+    },
+    mounted() {
+      this.$nextTick(() => {
+        const swiperTop = this.$refs.swiperTop.swiper
+        const swiperThumbs = this.$refs.swiperThumbs.swiper
+        swiperTop.controller.control = swiperThumbs
+        swiperThumbs.controller.control = swiperTop
+      })
     }
   }
 </script>
 
 <style lang="scss" scoped>
-
+  .swiper-container {
+    background-color: #ffffff;
+  }
+  .swiper-slide {
+    background-size: cover;
+    background-position: center;
+    &.slide-1 {
+      background-image:url('/static/images/arch.jpg');
+    }
+    &.slide-2 {
+      background-image:url('/static/images/arch-2.jpg');
+    }
+    &.slide-3 {
+      background-image:url('/static/images/hyatt.jpg');
+    }
+  }
+  .gallery-top {
+    height: 80%!important;
+    width: 100%;
+  }
+  .gallery-thumbs {
+    height: 20%!important;
+    box-sizing: border-box;
+    padding: 10px 0;
+  }
+  .gallery-thumbs .swiper-slide {
+    width: 25%;
+    height: 100%;
+    opacity: 0.4;
+  }
+  .gallery-thumbs .swiper-slide-active {
+    opacity: 1;
+  }
 </style>
