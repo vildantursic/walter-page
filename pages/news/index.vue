854155124e5763651db0d47b8e502927b8c1b91c
dragutin-avdalovic <dragutin.avdalovic@walter.ba>
2018-04-23 15:16:05 +0200

    WBNP-35 Connecting the api with news page, pagination added to the news page

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -3,7 +3,8 @@
     <AppPageTitle v-if="page.acf" :supertitle="page.acf.tease" :title="page.acf.title" :subtitle="page.acf.description" ></AppPageTitle>
     <AppFilter :filters="filters" :filterActive="2" :showDateFilter="true" :monthActive="2"></AppFilter>
     <div class="items">
-      <AppNews v-for="(item, index) of items"  :item="item" :key="index"/>
+      <AppNews v-for="(item, index) of limitBy(items, itemsToShow)" :key="index" :item="item"/>
+      <AppMoreCard v-if="items.length > itemsToShow" :numberOfItems="items.length - itemsToShow" @onShowMore="() => itemsToShow += itemsToShow"/>
     </div>
   </section>
 </template>
@@ -13,20 +14,23 @@
   import AppNews from '~/components/AppNews'
   import AppPageTitle from '~/components/AppPageTitle'
   import AppContactBox from '~/components/AppContactBox'
+  import AppMoreCard from '~/components/AppMoreCard'
   import axios from 'axios'
 
   export default {
     data() {
       return {
+        itemsToShow: 3,
         page: {
           acf: {}
         },
         items: [],
         filters: [
-          { id: 1, name: 'test 1' },
-          { id: 2, name: 'test 2' },
-          { id: 3, name: 'test 3' },
-          { id: 4, name: 'test 4' }
+          { id: 1, name: 'All' },
+          { id: 2, name: 'BIM Consulting and Engineering' },
+          { id: 3, name: 'BIM Modelling' },
+          { id: 4, name: 'BIM Asset Creation' },
+          { id: 5, name: 'Software Development' }
         ]
       }
     },
@@ -34,7 +38,8 @@
       AppFilter,
       AppNews,
       AppPageTitle,
-      AppContactBox
+      AppContactBox,
+      AppMoreCard
     },
     methods: {
       getImageSource(item) {
