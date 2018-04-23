808dd2b8fe3eed2df24ae14c3ea835f5e04db929
Vildan Tursic <vildantursic@hotmail.com>
2018-04-23 16:49:28 +0200

    implementing single case view, navigation on services, minor style updates

--- components/AppSingle.vue
+++ components/AppSingle.vue
@@ -1,33 +1,38 @@
 <template>
   <div class="card">
-    <div class="card-img-container">
-      <AppSlider :images="images"></AppSlider>
-      <!--<img class="card-img" src="~/static/images/hyatt.jpg" alt="">-->
+    <div class="close">
+      <i class="fas fa-times" @click="closeCase"></i>
     </div>
-    <div class="info-card">
-      <p class="category">BIM Consulting and Engineering</p>
-      <h1 class="title">Hyatt Regency Maraska
-        Zadar Hotel</h1>
-      <a class="link" href="https://www.symetri.com/plm">www.symetri.com/plm</a>
-      <p class="category">Agrob Buchtal GmbH</p>
-      <div class="text-box">
-        <p class="description">I'm a paragraph. Click here to add your
-          own text and edit me. It’s easy. Just
-          click “Edit Text” or double click me and
-          you can start adding your own content
-          and make changes to the font... I'm a paragraph. Click here to add your
-          own text and edit me. It’s easy. Just
-          click “Edit Text” or double click me and
-          you can start adding your own content
-          and make changes to the font...I'm a paragraph. Click here to add your
-          own text and edit me. It’s easy. Just
-          click “Edit Text” or double click me and
-          you can start adding your own content
-          and make changes to the font...</p>
+    <div class="content">
+      <div class="card-img-container">
+        <AppSlider :images="images"></AppSlider>
+        <!--<img class="card-img" src="~/static/images/hyatt.jpg" alt="">-->
       </div>
-       <div class="divider">
-         <p class="author">In Collaboration with Symetri</p>
-       </div>
+      <div class="info-card">
+        <p class="category">BIM Consulting and Engineering</p>
+        <h1 class="title">Hyatt Regency Maraska
+          Zadar Hotel</h1>
+        <a class="link" href="https://www.symetri.com/plm">www.symetri.com/plm</a>
+        <p class="category">Agrob Buchtal GmbH</p>
+        <div class="text-box">
+          <p class="description">I'm a paragraph. Click here to add your
+            own text and edit me. It’s easy. Just
+            click “Edit Text” or double click me and
+            you can start adding your own content
+            and make changes to the font... I'm a paragraph. Click here to add your
+            own text and edit me. It’s easy. Just
+            click “Edit Text” or double click me and
+            you can start adding your own content
+            and make changes to the font...I'm a paragraph. Click here to add your
+            own text and edit me. It’s easy. Just
+            click “Edit Text” or double click me and
+            you can start adding your own content
+            and make changes to the font...</p>
+        </div>
+        <div class="divider">
+          <p class="author">In Collaboration with Symetri</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -46,65 +51,85 @@
           'static/images/hyatt.jpg'
         ]
       }
+    },
+    methods: {
+      closeCase() {
+        this.$emit('onCloseCase')
+      }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-  .card{
-    display: flex;
-    flex-direction: row;
-    height: 500px;
+  .card {
+    position: fixed;
+    z-index: 100;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: white;
     -moz-border-image: -moz-linear-gradient(45deg, #405dce 0%, #8b20c0 100%);
     -webkit-border-image: -webkit-linear-gradient(45deg, #405dce 0%, #8b20c0 100%);
     border-image: linear-gradient(45deg, #405dce 0%, #8b20c0 100%);
     border-image-slice: 1;
-    .card-img-container
-    {
+
+    .close {
+      width: 100%;
+      height: 100px;
+
+      i {
+        font-size: 2em;
+      }
+    }
+
+    .content {
       display: flex;
-      justify-content: center;
-      align-items: center;
-      width: 60%;
-    }
-    .info-card
-    {
-      position: relative;
-      width: 40%;
-      padding: 5em 1em 1em 1.5em;
-      .category
-      {
-        margin-top: 1em;
+
+      .card-img-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 60%;
+      }
+      .info-card {
+        position: relative;
+        width: 40%;
+        padding: 5em 1em 1em 1.5em;
+
+        .category {
+          margin-top: 1em;
+          font-size: 0.75rem;
+          font-weight: 300;
+        }
+        .author {
+          /*margin: 0;*/
+          font-size: 0.8em;
+          font-weight: 500;
+          color: gray;
+          font-style: italic;
+        }
+        .description {
+          font-size: 0.8em;
+          color: gray;
+        }
+        .divider {
+          border-top: 1px solid gray;
+          width: 70%;
+          position: absolute;
+          bottom: 0;
+        }
+        .text-box {
+          height: 200px;
+          width: 100%;
+          overflow: auto;
+        }
+      }
+      .link {
         font-size: 0.75rem;
-        font-weight: 300;
+        text-decoration: none;
+        margin-bottom: 1em;
       }
-      .author
-      {
-        /*margin: 0;*/
-        font-size: 0.8em;
-        font-weight: 500;
-        color: gray;
-        font-style: italic;
-      }
-      .description{
-        font-size: 0.8em;
-        color: gray;
-      }
-      .divider{
-        border-top: 1px solid gray;
-        width: 70%;
-        position: absolute;
-        bottom: 0;
-      }
-      .text-box{
-        height: 200px;
-        width: 100%;
-        overflow: auto;
-      }
-    }
-    .link{
-      font-size: 0.75rem;
-      text-decoration: none;
-      margin-bottom: 1em;
     }
   }
 
