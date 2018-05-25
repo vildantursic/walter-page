159b91de97df0384cba2cc187e556bd4d632249e
dragutin-avdalovic <dragutin.avdalovic@walter.ba>
2018-05-25 09:25:01 +0200

    wbnp-107 Scholarship page width of post...

--- pages/scholarships/_id.vue
+++ pages/scholarships/_id.vue
@@ -139,6 +139,21 @@
     .post-left {
       width: 70%;
       padding: 0 2% 2% 2%;
+      @include screen-size('s')
+      {
+        width: 100%;
+      }
+      @include screen-size('m')
+      {
+        width: 100%;
+      }
+      @include screen-size('xs')
+      {
+        width: 100%;
+      }
+      .header-left {
+        font-weight: 800;
+      }
 
       .header-left {
         font-weight: 800;
