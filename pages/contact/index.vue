aa2b354e1ee88ed522905c2896e9634a1389cd53
dragutin-avdalovic <dragutin.avdalovic@walter.ba>
2018-07-25 16:06:58 +0200

    #WDD-74 address solved.

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -3,7 +3,7 @@
     <div class="contact">
       <div class="countries">
         <h3 class="text">Stockholm, Sweden</h3>
-        <h3 class="text">Sarajevo, Bosnia and Herzegovina</h3>
+        <h3 class="text">Muhameda Kantardžića 3,<br>Sarajevo, Bosnia and Herzegovina</h3>
       </div>
       <div class="map">
         <AppMap/>
